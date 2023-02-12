45bc1b64cbff1b2674a5a721916ca8ae2ea47699
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-02-11 20:51:00 -0700

    Added Jump, Shooting, and GM

--- MyHeroBorn/Assets/Scripts/ItemBehavior.cs
+++ MyHeroBorn/Assets/Scripts/ItemBehavior.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+    
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+
+            gameManager.Items += 1;
         }
     }
 }
