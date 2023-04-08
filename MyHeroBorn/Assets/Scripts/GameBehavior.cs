4e22e789e58d43a1afb0adad2a4d68fcff159ab2
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-04-07 18:40:11 -0700

    Updated Start/End, UI, etc & Build

--- MyHeroBorn/Assets/Scripts/GameBehavior.cs
+++ MyHeroBorn/Assets/Scripts/GameBehavior.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameBehavior : MonoBehaviour
 {
-    public bool showWinScreen = false;
-    public bool showLossScreen = false;
-    //UI Code
-    public string labelText = "Shelve all 4 books and win!";
+    // public bool showWinScreen = false;
+    // public bool showLossScreen = false;
+    // //UI Code
+    // public string labelText = "Shelve all 4 books and win!";
     public int maxItems = 4;
 
+    public TMP_Text HealthText;
+    public TMP_Text ItemText;
+    public TMP_Text ProgressText;
+
+    void Start()
+    {
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
+    }
+    
+    public Button WinButton;
     private int _itemsCollected = 0;
     public int Items 
     {
@@ -18,16 +31,15 @@ public class GameBehavior : MonoBehaviour
         set 
         { 
             _itemsCollected = value;
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+            ItemText.text = "Items Collected: " + Items;
             if(_itemsCollected >= maxItems)
             {
-                labelText = "You've shelved all the books!";
-                showWinScreen = true;
-                Time.timeScale = 0f;
+                ProgressText.text = "You've shelved all the books!";
+                WinButton.gameObject.SetActive(true);
             }
             else
             {
-                labelText = "Book shelved, only " + (maxItems - _itemsCollected) + " more to go!";
+                ProgressText.text = "Book shelved, only " + (maxItems - _itemsCollected) + " more to go!";
             }
         }
     }
@@ -38,42 +50,15 @@ public class GameBehavior : MonoBehaviour
         get { return _playerHP; }
         set {
             _playerHP = value;
-            if(_playerHP <= 0)
-            {
-                labelText = "Better luck next time!";
-                showLossScreen = true;
-                Time.timeScale = 0;
+            HealthText.text = "Player Health: " + HP;
+            ProgressText.text = "Shh... You're in a Library.";
             }
-            else
-            {
-                labelText = "Shh... You're in a Library.";
-            }
-        }
+        
     }
     void RestartLevel()
     {
         SceneManager.LoadScene(0);
         Time.timeScale = 1.0f;
     }
-    void OnGUI()
-    {
-        GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerHP);
-        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
-        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
-
-        if(showWinScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "YOU WON!!"))
-            {
-                RestartLevel();
-            }
-        }
-        if(showLossScreen)
-        {
-            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "You Lose..."))
-            {
-                RestartLevel();
-            }
-        }
-    }
+    
 }
